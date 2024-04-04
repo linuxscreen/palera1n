8bb600aaf9e8aa1905a22580dc9534b36e1f53b2
linuxscreen <trump2maga@outlook.com>
2024-04-04 23:21:41 +0800

    update build.yaml

--- src/main.c
+++ src/main.c
@@ -135,8 +135,8 @@ int palera1n(int argc, char *argv[]) {
 	if (!(palerain_flags & palerain_option_device_info))
 		LOG(LOG_INFO, "Waiting for devices");
 
-	if (access("/var/run/usbmuxd", F_OK) != 0) 
-		LOG(LOG_WARNING, "/var/run/usbmuxd not found, normal mode device detection will not work.");
+	if (access("/data/usbmuxd", F_OK) != 0) 
+		LOG(LOG_WARNING, "/data/usbmuxd not found, normal mode device detection will not work.");
 	
 	pthread_create(&pongo_thread, NULL, pongo_helper, NULL);
 	pthread_create(&dfuhelper_thread, NULL, dfuhelper, NULL);
